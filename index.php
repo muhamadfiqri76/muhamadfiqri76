3311a284e0da5439e9096a1f91de2ff8941afe16
muhamadfiqri76 <96520338+muhamadfiqri76@users.noreply.github.com>
2021-12-22 16:06:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="en"> 
+ <head> 
+  <meta charset="UTF-8"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Favicon --> 
+  <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon"> <!-- Font Awesome --> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"> <!-- AOS --> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"> <!-- Glidejs --> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/css/glide.core.css"> 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> <!-- Custom StyleSheet --> 
+  <link rel="stylesheet" href="styles.css"> 
+  <title>CURRICULUM VITAE</title> 
+ </head> 
+ <body> <!-- Header --> 
+  <header id="home" class="header"> <!-- Navigation --> 
+   <nav class="nav"> 
+    <div class="navigation container"> 
+     <div class="logo"> 
+      <h1>CURRICULUM VITAE<span>.</span></h1> 
+     </div> 
+     <div class="menu"> 
+      <div class="top-nav"> 
+       <div class="logo"> 
+        <h1>Fiqri</h1><span>.</span> 
+       </div> 
+       <div class="close"> <i class="fas fa-times"></i> 
+       </div> 
+      </div> 
+      <ul class="nav-list"> 
+       <li class="nav-item"> <a href="#home" class="nav-link scroll-link">Home</a> </li> 
+       <li class="nav-item"> <a href="#about" class="nav-link scroll-link">About</a> </li> 
+       <li class="nav-item"> <a href="#services" class="nav-link scroll-link">Services</a> </li> 
+       <li class="nav-item"> <a href="#skills" class="nav-link scroll-link">Skills</a> </li> 
+       <li class="nav-item"> <a href="#gallery" class="nav-link scroll-link">Gallery</a> </li> 
+      </ul> 
+     </div> 
+     <div class="hamburger"> <i class="fas fa-bars"></i> 
+     </div> 
+    </div> 
+   </nav> <!-- Hero --> 
+   <div class="hero"> 
+    <h1>Muhamad Fiqri</h1> 
+    <h4>Dan Saya Adalah <span id="type1"></span></h4> 
+   </div> 
+   <img class="banner" src="./images/chika-dance.gif" alt=""> 
+   <div class="icons"> <span><a href="https://wa.me/6289697491160" target="_blank"><i class="fab fa-whatsapp"></i></a></span> <span><a href="https://www.instagram.com/luthfi_sadli/" target="_blank"><i class="fab fa-instagram"></i></a></span> 
+   </div> 
+  </header> <!-- Main --> 
+  <section class="section about" id="about"> 
+   <div class="title"> 
+    <h1>About me</h1> 
+   </div> 
+   <div class="about-center container"> 
+    <div class="left" data-aos="fade-right" data-aos-duration="2000"> 
+     <img src="./images/fujiwara.gif" alt=""> 
+    </div> 
+    <div class="right" data-aos="fade-left" data-aos-duration="2000"> 
+     <h1>Nama Lengkap : Muhamad Fiqri</h1> 
+     <h1>Saya Adalah : <span id="type2"></span></h1> 
+     <p> Tempat, Tgl Lahir : Tangerang, 09 April 2004 <br> Alamat : Jalan Gambiran No 11 <br> Status : Siswa <br> NO HP : 085717657252 <br> Email : muhamadfiqri7680@gmail.com <br> </p> 
+    </div> 
+   </div> 
+  </section> 
+  <section class="section life-history" id="life"> <!-- life history start --> 
+   <div class="container"> 
+    <div class="row"> 
+     <div class="col-lg-6 col-12" data-aos="fade-right" data-aos-duration="2000"> 
+      <h2>Experiences</h2> 
+      <div class="life-single"> 
+       <p><span>2020 - Now</span></p> 
+       <h4>Fulltime Freelancer</h4> 
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odit itaque, distinctio sunt repudiandae cum excepturi eos ipsam necessitatibus voluptatibus delectus, quasi id tenetur accusantium ea vitae nobis molestias esse.</p> 
+      </div> 
+      <div class="life-single"> 
+       <p><span>2014 - 2017</span></p> 
+       <h4>Creative Director at Pixar Studio</h4> 
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odit itaque, distinctio sunt repudiandae cum excepturi eos ipsam necessitatibus voluptatibus delectus, quasi id tenetur accusantium ea vitae nobis molestias esse.</p> 
+      </div> 
+      <div class="life-single"> 
+       <p><span>2010 - 2014</span></p> 
+       <h4>Web Designer at Microsoft</h4> 
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odit itaque, distinctio sunt repudiandae cum excepturi eos ipsam necessitatibus voluptatibus delectus, quasi id tenetur accusantium ea vitae nobis molestias esse.</p> 
+      </div> 
+     </div> 
+     <div class="col-lg-6 col-12" data-aos="fade-left" data-aos-duration="2000"> 
+      <h2>Education</h2> 
+      <div class="life-single"> 
+       <p><span>2010</span></p> 
+       <h4>Master of Design, University of California</h4> 
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odit itaque, distinctio sunt repudiandae cum excepturi eos ipsam necessitatibus voluptatibus delectus, quasi id tenetur accusantium ea vitae nobis molestias esse.</p> 
+      </div> 
+      <div class="life-single"> 
+       <p><span>2007</span></p> 
+       <h4>Bachelor of Arts, Standford University</h4> 
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odit itaque, distinctio sunt repudiandae cum excepturi eos ipsam necessitatibus voluptatibus delectus, quasi id tenetur accusantium ea vitae nobis molestias esse.</p> 
+      </div> 
+      <div class="life-single"> 
+       <p><span>2004</span></p> 
+       <h4>Specialize App Design, Harvard University</h4> 
+       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odit itaque, distinctio sunt repudiandae cum excepturi eos ipsam necessitatibus voluptatibus delectus, quasi id tenetur accusantium ea vitae nobis molestias esse.</p> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+  </section> <!-- Services --> 
+  <section class="section theme" id="services"> 
+   <div class="title"> 
+    <h1>My Services</h1> 
+   </div> 
+   <div class="services-center container"> 
+    <div class="service" data-aos="fade-down" data-aos-duration="2000"> <span><i class="fab fa-accusoft"></i></span> 
+     <h2>Web Design</h2> 
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore exercitationem numquam porro asperiores neque architecto. </p> 
+    </div> 
+    <div class="service" data-aos="fade-up" data-aos-duration="2000"> <span><i class="fas fa-chart-line"></i></span> 
+     <h2>Advertising</h2> 
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore exercitationem numquam porro asperiores neque architecto. </p> 
+    </div> 
+    <div class="service" data-aos="fade-down" data-aos-duration="2000"> <span><i class="fas fa-blender-phone"></i></span> 
+     <h2>Apps Design</h2> 
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore exercitationem numquam porro asperiores neque architecto. </p> 
+    </div> 
+   </div> 
+  </section> <!-- Skills --> 
+  <section class="section skills" id="skills"> 
+   <div class="title"> 
+    <h1>My Skills</h1> 
+   </div> 
+   <div class="skills-center container"> 
+    <div class="skills-left" data-aos="fade-right" data-aos-duration="2000"> 
+     <div class="skills-box"> 
+      <h4>HTML</h4> 
+      <div class="skills-ilt"> 
+       <div class="skills-bar html"></div> <span>70%</span> 
+      </div> 
+     </div> 
+     <div class="skills-box"> 
+      <h4>CSS</h4> 
+      <div class="skills-ilt"> 
+       <div class="skills-bar css"></div> <span>70%</span> 
+      </div> 
+     </div> 
+     <div class="skills-box"> 
+      <h4>JavaScript</h4> 
+      <div class="skills-ilt"> 
+       <div class="skills-bar javascript"></div> <span>70%</span> 
+      </div> 
+     </div> 
+     <div class="skills-box"> 
+      <h4>PHP</h4> 
+      <div class="skills-ilt"> 
+       <div class="skills-bar php"></div> <span>70%</span> 
+      </div> 
+     </div> 
+     <div class="skills-box"> 
+      <h4>Java</h4> 
+      <div class="skills-ilt"> 
+       <div class="skills-bar java"></div> <span>70%</span> 
+      </div> 
+     </div> 
+    </div> 
+    <div class="right" data-aos="fade-left" data-aos-duration="2000"> 
+     <h3>My creative skills and experience</h3> 
+     <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam tempora distinctio sed. Animi, doloribus est. Suscipit praesentium tenetur libero eos! Suscipit voluptatem qui, nulla accusantium deleniti nam recusandae officiis possimus at voluptate cupiditate soluta nostrum deserunt consequuntur, ut quis eligendi? Quidem et doloremque cupiditate veniam optio corporis voluptatum, animi nemo! </p> 
+    </div> 
+   </div> 
+  </section> 
+  <section class="section gallery theme" id="gallery"> <!-- gallery start --> 
+   <div class="title"> 
+    <h1>Gallery</h1> 
+   </div> 
+   <div class="row images container"> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g1.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g2.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g3.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g4.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g5.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g6.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g7.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+    <div class="imgBox col-md-4 col-12"> 
+     <div> 
+      <img src="images/g8.jpg" alt="Gallery"> 
+     </div> 
+    </div> 
+   </div> 
+  </section><!-- gallery end --> <!-- AOS --> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script> <!-- Typeit --> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/typeit/7.0.4/typeit.min.js"></script> <!-- GSAP --> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script> <!-- Glidejs --> 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.min.js"></script> <!-- Custom Script --> 
+  <script src="index.js"></script> 
+ </body>
+</html>
